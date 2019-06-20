301826e5cb4be01cc5abad5784de6e3feb4d2154
odyksa <odyksa@myagent.com.ua>
2019-06-20 17:30:18 +0300

    add component Recipe

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         ev.preventDefault();
         const recipeValue = ev.target.elements.recipeValue.value;
         const API_KEY = 'a40b8bb0c61f559d98cc93ce9b938549';
-        const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeValue}&count=10`);
+        const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeValue}&count=9`);
 
         const data = await api_call.json();
         this.setState({ recipes: data.recipes });
--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -5,8 +5,27 @@ export default function Recipes(props) {
     const recipes = props.recipes.map(recipe => {
         return (
             <div className="recipe" key={recipe.recipe_id}>
-                <h3 className="recipe__title">{recipe.title}</h3>
-                <img src={recipe.image_url} alt={recipe.title} className="recipe__img" />
+                <h3 
+                    className="recipe__title"
+                >
+                    {
+                        recipe.title.length > 40 ? `${recipe.title.substring(0, 39)}...` : `${recipe.title}`
+                    }
+                </h3>
+                <img 
+                    src={recipe.image_url} 
+                    alt={recipe.title} 
+                    className="recipe__img" 
+                />
+                <footer className="recipe__footer">
+                    <p>
+                        Publisher: 
+                        <span>
+                            {recipe.publisher.length > 25 ? `${recipe.publisher.substring(0, 24)}...` : `${recipe.publisher}`}
+                        </span>
+                    </p>
+                    <button className="recipe__details">View recipe</button>
+                </footer>
             </div>
         );
     });
